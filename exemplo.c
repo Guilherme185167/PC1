d269966530f693a2177984d4b6ad47d4ec8a2e62
Guilherme <rgm54586@comp.uems.br>
2025-05-21 20:16:10 -0400

    alteração de menu, sons, opçoes de livros e começo do desenvolvimento dos livros web

--- exemplo.c
+++ exemplo.c
@@ -1,3 +1,11 @@
-int main(){
-    
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        printf("\a");
+        fflush(stdout); // força a saída imediata
+        sleep(1);
+    }
+    return 0;
 }
--- trabalho/main.c
+++ trabalho/main.c
@@ -8,6 +8,12 @@
 #define tit 30
 struct termios original;
 
+void menu_principal();
+void menu_livro();
+
+char *nome_livro[] = {
+    "Hardware"};
+
 void desativarModoCanonico()
 {
     struct termios novo;
@@ -22,7 +28,7 @@ void restaurarModoCanonico()
     tcsetattr(STDIN_FILENO, TCSANOW, &original);
 }
 
-void livro1()
+void livros(int n, int gabarito[10])
 {
     int mv = 0, que = 0, resp[10], cer = 0, err = 0, form[10];
     char nav;
@@ -32,19 +38,18 @@ void livro1()
         "\033[33m", // Amarelo
         "\033[34m"  // Azul
     };
-    int gab[10] = {1, 1, 4, 3, 2, 4, 1, 1, 4, 3};
 
     puts("Utilize o livro para visualizar as questoes!\n");
     while (1)
     {
         system("clear");
-        printf("\033[1;33mNa cozinha: Questão %d [a/d para mover]\033[m\n\n", que + 1);
+        printf("\t\t\033[1;33m%s: Questão %d [a/d para mover q para sair]\033[m\n\n", nome_livro[n], que + 1);
         for (int i = 0; i < 4; i++)
         {
             if (i == mv)
-                printf(" > \033[%s\u2588\u2588\033[m < ", cores[i]); // destaque reverso
+                printf("\t\a|| \033[%s\u2588\u2588\033[m ||\t", cores[i]); // destaque reverso
             else
-                printf(" \033[%s\u2588\u2588\033[m ", cores[i]); // normal
+                printf(" \t\a\033[%s\u2588\u2588\033[m\t ", cores[i]); // normal
         }
         puts("");
 
@@ -66,16 +71,18 @@ void livro1()
             resp[que] = mv + 1;
             que += 1;
         }
+        else if(nav == 'q'){
+            menu_livro();
+        }
         if (que == 10)
         {
             break;
         }
     }
-
     // verificação das respostas
     for (int j = 0; j < 10; j++)
     {
-        if (resp[j] == gab[j])
+        if (resp[j] == gabarito[j])
         {
             cer += 1;
             form[j] = 1;
@@ -98,45 +105,45 @@ void livro1()
     // imprimir gabarito
     for (int j = 0; j < 10; j++)
     {
-        printf("\033[32m%d \033[m", gab[j]);
+        printf("\033[32m%d \033[m", gabarito[j]);
     }
     puts("\n");
     printf("\033[32mAcertos: %d \033[31mErros %d\n\033[m", cer, err);
 }
+
 void menu_livro()
 {
     int mv = 0;
     char nav;
     // opções do menu
     char *op[] = {
-        "Atividades Programadas Nº 01",
-        "Atividades Programadas Nº 02",
-        "Atividades Programadas Nº 03",
-        "Sair"};
+        "\t\t\t\aAtividades Programadas Nº 01",
+        "\t\t\t\aAtividades Programadas Nº 02",
+        "\t\t\t\aAtividades Programadas Nº 03",
+        "\t\t\t\a::Sair"};
 
     while (1)
     {
         system("clear");
-        puts("\033[1;33mEscolha um livro: [use w/s para navegar enter para confirmar]\033[m\n");
-        for (int i = 0; i < 3; i++)
+        puts("\033\t[1;33mEscolha um livro: [use w/s para navegar enter para confirmar]\033[m\n");
+        for (int i = 0; i < 4; i++)
         {
             if (i == mv)
-                printf("\033[7;37m%s\033[m\n", op[i]);
+                printf("\033[1;36;47m%s\033[m\n", op[i]);
             else
                 printf("%s\n", op[i]);
         }
-
         nav = getchar();
         if (nav == 'w')
         {
             mv--;
             if (mv < 0)
-                mv = 2;
+                mv = 3;
         }
         else if (nav == 's')
         {
             mv++;
-            if (mv > 2)
+            if (mv > 3)
                 mv = 0;
         }
         else if (nav == '\n')
@@ -147,15 +154,86 @@ void menu_livro()
     switch (mv)
     {
     case 0:
-        livro1();
+    {
+        
+        int gab1[10] = {1, 1, 4, 3, 2, 4, 1, 1, 4, 3};
+        livros(0, gab1);
+        break;
+    }
+    case 1:
+    {
+        int gab2[10] = {2, 1, 4, 3, 2, 4, 1, 1, 4, 3};
+        livros(0, gab2);
+        break;
+    }
+    case 2:
+    {
+        int gab3[10] = {3, 1, 4, 3, 2, 4, 1, 1, 4, 3};
+        livros(0, gab3);
+        break;
+    }
+    case 3:
+        menu_principal();
+        break;
+
+    default:
+        break;
+    }
+}
+void menu_principal()
+{
+    int mv = 0;
+    char nav;
+    // opções do menu
+    char *op[] = {
+        "\t\t\t\aEquações Matematicas",
+        "\t\t\t\aLivros",
+        "\t\t\t\aGabritos",
+        "\t\t\t\a::Sair"};
+
+    while (1)
+    {
+        system("clear");
+        puts("\033\t[1;33mEscolha um desafio: [use w/s para navegar enter para confirmar]\033[m\n");
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == mv)
+                printf("\033[1;36;47m%s\033[m\n", op[i]);
+            else
+                printf("%s\n", op[i]);
+        }
+        nav = getchar();
+        if (nav == 'w')
+        {
+            mv--;
+            if (mv < 0)
+                mv = 3;
+        }
+        else if (nav == 's')
+        {
+            mv++;
+            if (mv > 3)
+                mv = 0;
+        }
+        else if (nav == '\n')
+        {
+            break;
+        }
+    }
+    switch (mv)
+    {
+    case 0:
+        /* code */
         break;
     case 1:
-        /* code */
+        menu_livro();
         break;
     case 2:
         /* code */
         break;
-
+    case 3:
+        break;
+    
     default:
         break;
     }
@@ -163,8 +241,8 @@ void menu_livro()
 
 int main()
 {
-    desativarModoCanonico(); // Ativa modo de leitura direta do teclado
-    menu_livro();            // Executa o menu
-    restaurarModoCanonico(); // Restaura terminal ao final
+    desativarModoCanonico();
+    menu_principal();
+    restaurarModoCanonico();
     return 0;
 }
